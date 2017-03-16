e962c4b6b43cbb8a9c5d04bd35fde3166f5bf99d
Jackson-Baker-1617 <jacksonlb@bendbroadband.com>
2017-03-16 11:06:11 -0700

    Add files via upload

--- turdlehitler.py
+++ turdlehitler.py
@@ -1,37 +1,18 @@
 from turtle import *
 
-#create the loop doop
-def superdooper():
-    forward(10)
-    left(90)
-    forward(70)
-    left(90)
-    forward(70)
-    left(90)
-    forward(70)
-    left(90)
-    forward(10)
-    left(90)
-    forward(49)
-    right(90)
-    forward(48)
-    right(90)
-    forward(49)
+forward(10)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(10)
+left(90)
+forward(49)
+right(90)
+forward(48)
+right(90)
+forward(49)
 
-#Move to the left
-def rotation():
-    penup()
-    right(180)
-    left(90)
-    forward(129)
-    right(90)
-    forward(10)
-    left(180)
-    pendown()
-#DO IT JUST DO IT DONT LET YOUR DREAMS BE DREAMS
-def doit():
-    for i in range(4):
-        superdooper()
-        rotation()
-#actually running it
-doit()
