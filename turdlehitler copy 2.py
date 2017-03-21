76e31110bd9a2fb05cfc13b35dc6718838c92ca9
Jackson-Baker-1617 <jacksonlb@bendbroadband.com>
2017-03-21 10:58:44 -0700

    Add files via upload

---
+++ turdlehitler copy 2.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+forward(10)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(10)
+left(90)
+forward(49)
+right(90)
+forward(48)
+right(90)
+forward(49)
+
